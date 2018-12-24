3500f60e2060e2afbd7aaba26b8e7f30262bbf21
EC2 Default User <stella.incident@gmail.com>
2018-12-24 05:29:52 +0000

    update routes and add def show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  
-  get 'users/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
@@ -12,4 +10,6 @@ Rails.application.routes.draw do
   get '/contact',to:'static_pages#contact'
   
   get '/signup',to:'users#new'
+  
+  resources :users
 end
